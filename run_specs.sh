fc77af33f1c3ca77b7c449fbe6b7fee84f1f8666
Grant Petersen-Speelman <grantspeelman@gmail.com>
2016-05-17 15:13:15 +0200

    debug travis.yml

--- run_specs.sh
+++ run_specs.sh
@@ -1,6 +1,3 @@
 #!/usr/bin/env bash
-vagrant up
-vagrant provision
-vagrant ssh-config > ssh_config
-bundle exec rspec spec
+vagrant up && vagrant provision && vagrant ssh-config > ssh_config && bundle exec rspec spec
 
