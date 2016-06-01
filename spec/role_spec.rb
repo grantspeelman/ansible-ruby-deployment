dd6e6a1dd5c468312c4972c470758e1c3e580360
Grant Petersen-Speelman <grantspeelman@gmail.com>
2016-06-01 10:37:20 +0200

    fix mode for .env file to 644

--- spec/role_spec.rb
+++ spec/role_spec.rb
@@ -17,6 +17,12 @@ describe user('deploy') do
   it { should exist }
 end
 
+describe file('/home/deploy/.env') do
+  it { should be_file }
+  it { should contain('postgresql://localhost/production?pool=5') }
+  it { should be_mode(644) }
+end
+
 unless ENV['SPEC_BACKEND_EXEC']
   describe command('bundle --version') do
     let(:sudo_options) { '-u deploy -i' }
